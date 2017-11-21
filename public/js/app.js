0c053a87da6a4707343efbbc8c7fb9acfd39ec8e
vGknight <gregory.knight@outlook.com>
2017-11-21 07:35:02 -0500

    adding note functions

--- public/js/app.js
+++ public/js/app.js
@@ -9,9 +9,12 @@ $.getJSON("/articles", function(data) {
   }
 });
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", ".view-notes", function() {
   // Empty the notes from the note section
-  $("#notes").empty();
+  // $("#myModal").empty();
+
+
+  $('#myModal').modal('show');
   // Save the id from the p tag
   var thisId = $(this).attr("data-id");
   // Now make an ajax call for the Article
@@ -22,20 +25,28 @@ $(document).on("click", "p", function() {
     // With that done, add the note information to the page
     .done(function(data) {
       console.log(data);
+      $("#note-title").empty();
+      $("note-content").empty();
       // The title of the article
       $("#notes").append("<h2>" + data.headline + "</h2>");
+
+      // $("#notes").append("<h2>" + data.headline + "</h2>");
+
+      $('#note-title').append("<h5> Note for Article " + data._id + "</h5>");
       // An input to enter a new title
       $("#notes").append("<input id='titleinput' name='title' >");
       // A textarea to add a new note body
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      // $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      $("#add-note").attr("data-id", data._id);
       // If there's a note in the article
       if (data.note) {
         // Place the title of the note in the title input
         $("#titleinput").val(data.note.title);
         // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
+        $("#note-content").val(data.note.body);
       }
     });
 });
@@ -93,3 +104,92 @@ $(document).on("click", ".savearticle", function() {
   // $("#titleinput").val("");
   // $("#bodyinput").val("");
 });
+
+// When you click the savenote button
+$(document).on("click", ".scrapearticle", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "GET",
+    url: "/scrape"
+    // data: {
+    //   // Value taken from title input
+    //   title: $("#titleinput").val(),
+    //   // Value taken from note textarea
+    //   body: $("#bodyinput").val()
+    // }
+  })
+    // With that done
+    .done(function(data) {
+      // Log the response
+      console.log(data);
+      location.reload();
+      // Empty the notes section
+      // $("#notes").empty();
+    });
+  // Also, remove the values entered in the input and textarea for note entry
+  // $("#titleinput").val("");
+  // $("#bodyinput").val("");
+});
+
+
+
+
+// When you click the savenote button
+$(document).on("click", ".delete-saved-article", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/unsave/" + thisId
+  })
+    // With that done
+    .done(function(data) {
+      // Log the response
+      location.reload();
+      // Empty the notes section
+      // $("#notes").empty();
+    });
+  // Also, remove the values entered in the input and textarea for note entry
+  // $("#titleinput").val("");
+  // $("#bodyinput").val("");
+});
+
+// myForm
+
+$("#myForm").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+        console.log("submit pushed");
+        var thisId = $('#add-note').attr("data-id");
+        console.log(thisId);
+        alert(thisId);
+
+        event.preventDefault();
+
+        var newNote = {
+            body: $("#note-content").val().trim()
+        };
+
+        console.log(newNote);
+
+        // Send the POST request.
+        $.ajax("/articles/" + thisId , {
+            type: "POST",
+            data: newNote
+        }).then(
+            function() {
+                console.log("note Added...");
+                // Reload the page to get the updated list - this sucks
+                location.reload();
+
+
+            }
+        );
+    });
+
+
+
+
+
--- server.js
+++ server.js
@@ -16,20 +16,21 @@ var exphbs = require("express-handlebars");
 // app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.engine('handlebars', exphbs({
     defaultLayout: "main",
-    helpers:{
-      // Function to do basic mathematical operation in handlebar
-      math: function(lvalue, operator, rvalue) {lvalue = parseFloat(lvalue);
-          rvalue = parseFloat(rvalue);
-          return {
-              "+": lvalue + rvalue,
-              "-": lvalue - rvalue,
-              "*": lvalue * rvalue,
-              "/": lvalue / rvalue,
-              "%": lvalue % rvalue
-          }[operator];
-      }
+    helpers: {
+        // Function to do basic mathematical operation in handlebar
+        math: function(lvalue, operator, rvalue) {
+            lvalue = parseFloat(lvalue);
+            rvalue = parseFloat(rvalue);
+            return {
+                "+": lvalue + rvalue,
+                "-": lvalue - rvalue,
+                "*": lvalue * rvalue,
+                "/": lvalue / rvalue,
+                "%": lvalue % rvalue
+            }[operator];
+        }
 
-}
+    }
 }));
 app.set("view engine", "handlebars");
 var request = require("request");
@@ -37,25 +38,8 @@ var request = require("request");
 var cheerio = require("cheerio");
 
 
-
-// Database configuration
-// var databaseUrl = "scrapy";
-// var collections = ["scrapedData"];
-
-// Hook mongojs configuration to the db variable
-// var db = mongojs(databaseUrl, collections);
-// db.on("error", function(error) {
-//   console.log("Database Error:", error);
-// });
-
-
-
-
 // Require all models
 var db = require("./models");
-
-
-
 
 // Configure middleware
 // Use morgan logger for logging requests
@@ -74,10 +58,7 @@ mongoose.connect("mongodb://localhost/scrapy", {
 /* TODO: make two more routes
  * -/-/-/-/-/-/-/-/-/-/-/-/- */
 
-// Main route (simple Hello World Message)
-// app.get("/", function(req, res) {
-//     res.send("Hello world");
-// });
+
 
 app.get("/", function(req, res) {
     db.Article
@@ -99,14 +80,27 @@ app.get("/", function(req, res) {
             res.json(err);
         });
 
-    // burger.selectAll(function(data) {
-    // var hbsObject = {
-    //     burger: data
-    // };
-    // console.log(hbsObject);
+});
 
-    // res.render("index", hbsObject);
-    // });
+// Route for getting all saved Articles from the db
+app.get("/saved", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article
+        .find({ isSaved: true })
+        .then(function(dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            // res.json(dbArticle);
+            var hbsObject = {
+                article: dbArticle
+            };
+            console.log(hbsObject);
+
+            res.render("saved", hbsObject);
+        })
+        .catch(function(err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
 });
 
 
@@ -128,27 +122,12 @@ app.get("/scrape", function(req, res) {
 
         // With cheerio, look at each award-winning site, enclosed in "figure" tags with the class name "site"
         $(".SectionStoriesTeaser").each(function(i, element) {
-            // $("figure.rollover").each(function(i, element) {
-
-            /* Cheerio's find method will "find" the first matching child element in a parent.
-             *    We start at the current element, then "find" its first child a-tag.
-             *    Then, we "find" the lone child img-tag in that a-tag.
-             *    Then, .attr grabs the imgs srcset value.
-             *    The srcset value is used instead of src in this case because of how they're displaying the images
-             *    Visit the website and inspect the DOM if there's any confusion
-             */
             var result = {};
-            // var testingArr = [];
-
-            // var h3 = $(element).find("a");
             result.headline = $(element).find(".headline").find("a").text();
             result.summary = $(element).find(".summarytext")[0].children[0].data;
             result.link = $(element).find("a").attr('href');
             result.author = $(element).find(".author").text();
             result.relDate = $(element).find(".releaseDate").text().trim();
-            // result.isSaved = false;
-
-            console.log(result);
 
             var query = { headline: result.headline };
             var update = result;
@@ -160,7 +139,16 @@ app.get("/scrape", function(req, res) {
 
                 .then(function(dbArticle) {
                     // If we were able to successfully scrape and save an Article, send a message to the client
+                    // res.redirect("/");
+
+                    // var hbsObject = {
+                    //     article: dbArticle
+                    // };
+                    // console.log(hbsObject);
+
+                    // res.render("index");
                     res.send("Scrape Complete");
+
                 })
                 .catch(function(err) {
                     // If an error occurred, send it to the client
@@ -172,20 +160,6 @@ app.get("/scrape", function(req, res) {
     });
 
 
-    // After looping through each element found, log the results to the console
-    // console.log(html);
-    // console.log(results);
-    //             db.Article
-    //             .create(results)
-    //             .then(function(dbArticle) {
-    //                 // If we were able to successfully scrape and save an Article, send a message to the client
-    //                 res.send("Scrape Complete");
-    //             })
-    //             .catch(function(err) {
-    //                 // If an error occurred, send it to the client
-    //                 res.json(err);
-    //             });
-    //     });
 });
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
@@ -204,20 +178,7 @@ app.get("/articles", function(req, res) {
 
 
 
-// Route for getting all saved Articles from the db
-app.get("/saved", function(req, res) {
-    // Grab every document in the Articles collection
-    db.Article
-        .find({ isSaved: true })
-        .then(function(dbArticle) {
-            // If we were able to successfully find Articles, send them back to the client
-            res.json(dbArticle);
-        })
-        .catch(function(err) {
-            // If an error occurred, send it to the client
-            res.json(err);
-        });
-});
+
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
@@ -237,6 +198,7 @@ app.get("/articles/:id", function(req, res) {
 
 app.post("/articles/:id", function(req, res) {
     // Create a new note and pass the req.body to the entry
+    console.log(req.body);
     db.Note
         .create(req.body)
         .then(function(dbNote) {
@@ -260,13 +222,6 @@ app.post("/save/:id", function(req, res) {
     db.Article
 
         .findOneAndUpdate({ _id: req.params.id }, { isSaved: true })
-        // .create(req.body)
-        // .then(function(dbNote) {
-        //   // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        //   // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        //   // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        //   return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-        // })
         .then(function(dbArticle) {
             // If we were able to successfully update an Article, send it back to the client
             res.json(dbArticle);
@@ -278,73 +233,20 @@ app.post("/save/:id", function(req, res) {
 });
 
 
+app.post("/unsave/:id", function(req, res) {
+    // markes article id as saved
+    db.Article
 
-// Route 2
-// =======
-// When you visit this route, the server will
-// scrape data from the site of your choice, and save it to
-// MongoDB.
-// TIP: Think back to how you pushed website data
-// into an empty array in the last class. How do you
-// push it into a MongoDB collection instead?
-
-/* -/-/-/-/-/-/-/-/-/-/-/-/- */
-
-
-// request("https://www.clevescene.com/cleveland/dining/Section?oid=1392921", function(error, response, html) {
-
-//   // Load the HTML into cheerio
-//   var $ = cheerio.load(html);
-
-//   // Make an empty array for saving our scraped info
-//   var results = [];
-
-//   // With cheerio, look at each award-winning site, enclosed in "figure" tags with the class name "site"
-//   $(".SectionStoriesTeaser").each(function(i, element) {
-//    // $("figure.rollover").each(function(i, element) {
-
-//     /* Cheerio's find method will "find" the first matching child element in a parent.
-//      *    We start at the current element, then "find" its first child a-tag.
-//      *    Then, we "find" the lone child img-tag in that a-tag.
-//      *    Then, .attr grabs the imgs srcset value.
-//      *    The srcset value is used instead of src in this case because of how they're displaying the images
-//      *    Visit the website and inspect the DOM if there's any confusion
-//     */
-//     // var imgLink = $(element).find("a").find("img").attr("srcset").split(",")[0].split(" ")[0];
-//     var h3 = $(element).find("a");
-//     // var summary = $(element).find(".summarytext")[0].attr();
-//     var headline = $(element).find(".headline").find("a").text();
-//     var summary = $(element).find(".summarytext")[0].children[0].data;
-
-//     var link = $(element).find("a").attr('href');
-//     var auth = $(element).find(".author").text();
-//     var releaseDate = $(element).find(".releaseDate").text().trim();
-
-//     // Push the image's URL (saved to the imgLink var) into the results array
-//     results.push({ headline: headline, 
-//             summary: summary,
-//             author : auth,
-//             relDate: releaseDate,
-//             link: link
-//              });
-
-//      // db.scrapedData.insert({"Headline": headline,
-//      //             "Summary": summary,
-//      //             "RelDate": releaseDate,
-//      //             "link" : link });
-
-//   });
-
-
-//   // After looping through each element found, log the results to the console
-//   // console.log(html);
-//   console.log(results);
-// });
-
-// db.places.insert({"Headline": headline,
-//                  "Summary": summary,
-//                  "RelDate": releaseDate,
-//                  "link" : link });
+        .findOneAndUpdate({ _id: req.params.id }, { isSaved: false })
+        .then(function(dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 
 
 
